a3c842dba697570753c634c420ad83a46cfe8121
Kjgwo <woo052435@gmail.com>
2022-02-07 19:48:36 +0900

    22.02.07

---
+++ js/js.js
@@ -0,0 +1,8 @@
+//mobile nav
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
